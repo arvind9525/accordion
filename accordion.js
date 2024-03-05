8011f85837d16ddc656c99c72ead2d84e55a3be7
ARVIND SHARMA <54414078+arvind9525@users.noreply.github.com>
2024-03-05 11:14:40 +0530

    accordion code

---
+++ accordion.js
@@ -0,0 +1,12 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+<script>
+ 
+  $( function() {
+    $( "#row--22289 .col-inner" ).accordion({
+      heightStyle: "content",
+      active:true,
+      collapsible:true
+      
+    });
+  });
+</script>
